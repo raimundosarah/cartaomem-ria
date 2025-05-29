394d56e9044b3b7aa43d41ac90a8b6d461df4929
sarah <s.raimundo@escola.pr.gov.br>
2025-05-29 18:33:55 -0300

    Add files via upload

---
+++ flashcard/perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'Estrelas-do-mar', /*categoria*/
+    'Você sabia que as estrelas-do-mar enxergam o ambiente com olhos nas pontas dos braços?', /*pergunta*/
+    'Elas possuem olhos pequenos nas pontas dos braços que detectam luz e sombra, ajudando na orientação.' /*reposta*/
+)
+
+criaCartao(
+    'Cachalotes', /*categoria*/
+    'Você sabia que o som produzido pelo cachalote pode afetar outros animais?', /*pergunta*/
+    'Seus cliques alcançam até 230 decibéis, podendo causar desorientação e até desmaios em animais próximos.' /*reposta*/
+)
+
+criaCartao(
+    'Águas-vivas', /*categoria*/
+    'Você sabia que as águas-vivas não têm cérebro, coração nem ossos?', /*pergunta*/
+    'Elas sobrevivem porque usam uma rede simples de nervos para detectar estímulos, se mover e capturar presas, além de pulsar para nadar, tudo sem precisar de órgãos complexos.' /*reposta*/
+)
+
+criaCartao(
+    'Polvos', /*categoria*/
+     'Você sabia que o polvo utiliza seus braços para pensar e se movimentar?', /*pergunta*/
+    'Cada braço do polvo tem neurônios próprios que permitem movimentos independentes e respostas rápidas, funcionando quase como “mini cérebros”.' /*reposta*/
+)
+
+criaCartao(
+    'Peixe-morcego', /*categoria*/
+    'Você sabia que o peixe-morcego “anda” no fundo do mar usando suas nadadeiras?', /*pergunta*/
+    'Ele usa suas nadadeiras como pernas para se locomover pelo fundo do oceano.' /*reposta*/
+)
+
+criaCartao(
+    'Peixes Rápidos', /*categoria*/
+    'Você sabia que o peixe-agulha é o animal marinho mais rápido?', /*pergunta*/
+    'Ele pode atingir velocidades de até 110 km/h!' /*reposta*/
+)
+
+criaCartao(
+    'Tubarões', /*categoria*/
+    'Você sabia que o tubarão branco consegue sentir o coração das suas presas?', /*pergunta*/
+    ' Ele tem sensores especiais chamados ampolas de Lorenzini que detectam campos elétricos.' /*reposta*/
+)
+
+criaCartao(
+    'Cavalos-marinhos', /*categoria*/
+    'Você sabia que o macho do cavalo-marinho carrega os ovos até eles nascerem?', /*pergunta*/
+    'O macho tem uma bolsa onde os ovos ficam protegidos até a eclosão.' /*reposta*/
+)
+
+criaCartao(
+    'Peixe-gota', /*categoria*/
+    'Você sabia que o peixe-gota tem uma aparência “derretida”?', /*pergunta*/
+    'Ele vive em alta pressão nas profundezas e seu corpo mole perde forma fora da água.' /*reposta*/
+)
+
+criaCartao(
+    'Bioluminescência', /*categoria*/
+    'Você sabia que muitos animais marinhos emitem luz própria?', /*pergunta*/
+     'Essa luz, chamada bioluminescência, serve para comunicação, atração de presas e defesa contra predadores.' /*reposta*/
+)
+
+criaCartao(
+    'Golfinhos', /*categoria*/
+    'Você sabia que os golfinhos dormem com um olho aberto?', /*pergunta*/
+    'Eles mantêm metade do cérebro ativo enquanto dormem para continuar respirando e se proteger de predadores.' /*reposta*/
+)
+
+criaCartao(
+    'Caranguejos', /*categoria*/
+    'Você sabia que alguns caranguejos conseguem andar de lado e para frente?', /*pergunta*/
+    'A maioria dos caranguejos anda de lado, mas algumas espécies, como o caranguejo-ermitão, também conseguem se mover para frente e até para trás!' /*reposta*/
+)
